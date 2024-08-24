2a138aea4cc5166161e3ce2f8a3ec3eb766392ff
Basnayake B.Y.N <131932017+YasiruDEX@users.noreply.github.com>
2024-08-24 18:38:25 +0530

    update: roll back

--- personal-portfolio/app/page.tsx
+++ personal-portfolio/app/page.tsx
@@ -21,7 +21,7 @@ const Home = () => {
         <Experience />
         <RecentProjects />
         {/* <Clients /> */}
-        <Approach />
+        {/* <Approach /> */}
         <Footer />
       </div>
     </main>
